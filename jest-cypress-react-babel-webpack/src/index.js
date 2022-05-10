b084c2cdfe7851dcc44744fd8d0d25282336011f
mario-aj <mariojorge1997@mail.ru>
2022-05-10 15:08:42 +0100

    update react version

--- jest-cypress-react-babel-webpack/src/index.js
+++ jest-cypress-react-babel-webpack/src/index.js
@@ -1,9 +1,11 @@
 import './global.css'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import App from './app'
 
 if (module.hot) {
   module.hot.accept()
 }
-ReactDOM.render(<App />, document.getElementById('app'))
+
+const root = createRoot(document.getElementById('app'))
+root.render(<App />)
