da5a892a243e79440197850a365b3cce5c2e466b
mario-aj <mariojorge1997@mail.ru>
2022-05-13 16:51:59 +0100

    config jest to report code coverage

--- jest-cypress-react-babel-webpack/jest.config.js
+++ jest-cypress-react-babel-webpack/jest.config.js
@@ -9,4 +9,5 @@ module.exports = {
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['@emotion/jest/serializer'],
+  collectCoverageFrom: ['**/src/**/*.js'],
 }
