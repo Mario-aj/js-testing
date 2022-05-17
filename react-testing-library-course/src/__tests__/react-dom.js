723371a209479028586bb04a6ed94e1979879d41
mario-aj <mariojorge1997@mail.ru>
2022-05-17 16:40:09 +0100

    a custom render function

--- react-testing-library-course/src/__tests__/react-dom.js
+++ react-testing-library-course/src/__tests__/react-dom.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {getQueriesForElement} from '@testing-library/dom'
+import {getQueriesForElement, queries} from '@testing-library/dom'
 
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
+function render(ui) {
+  const container = document.createElement('div')
+  ReactDOM.render(ui, container)
 
-  const {getByLabelText} = getQueriesForElement(div)
+  const queries = getQueriesForElement(container)
+  return {container, ...queries}
+}
+
+test('renders a number with a label "Favorite Number"', () => {
+  const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
 
   expect(input).toHaveAttribute('type', 'number')
