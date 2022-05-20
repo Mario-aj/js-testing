1720741c5a6434b8b8152fb7b2c8caa74afd0a59
mario-aj <mariojorge1997@mail.ru>
2022-05-20 17:47:11 +0100

    Ensure error boundaries cann successfully recover from errors

---
+++ react-testing-library-course/src/__tests__/error-boundary-03.js
@@ -0,0 +1,67 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+
+import {reportError as mockReportError} from '../api'
+import {ErrorBoundary} from '../error-boundary'
+
+jest.mock('../api')
+
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterEach(jest.clearAllMocks)
+
+afterAll(() => {
+  console.error.mockRestore()
+})
+
+function Bomb({shoudlThrow}) {
+  if (shoudlThrow) {
+    throw new Error('Error thrown')
+  }
+  return null
+}
+
+test('render error boundary', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+  const {rerender, getByText, queryByRole, getByRole} = render(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb shoudlThrow={true} />
+    </ErrorBoundary>,
+  )
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledTimes(2)
+  expect(getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"There was a problem."`,
+  )
+
+  mockReportError.mockClear()
+  console.error.mockClear()
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  const tryAgainButton = getByText(/Try again/i)
+
+  fireEvent.click(tryAgainButton)
+
+  expect(mockReportError).not.toHaveBeenCalled()
+  expect(console.error).not.toHaveBeenCalled()
+  expect(queryByRole('alert')).not.toBeInTheDocument()
+  expect(tryAgainButton).not.toBeInTheDocument()
+})
