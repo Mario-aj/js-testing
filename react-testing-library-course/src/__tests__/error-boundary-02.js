933d572a116b4829c679629695a124a632ddfce0
mario-aj <mariojorge1997@mail.ru>
2022-05-20 17:46:32 +0100

    hide console error logs when testing error boundaries

---
+++ react-testing-library-course/src/__tests__/error-boundary-02.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+
+import {reportError as mockReportError} from '../api'
+import {ErrorBoundary} from '../error-boundary'
+
+jest.mock('../api')
+
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterEach(jest.clearAllMocks)
+
+afterAll(() => {
+  console.error.mockRestore()
+})
+
+function Bomb({shoudlThrow}) {
+  if (shoudlThrow) {
+    throw new Error('Error thrown')
+  }
+  return null
+}
+
+test('render error boundary', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+  const {rerender} = render(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb shoudlThrow={true} />
+    </ErrorBoundary>,
+  )
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+})
