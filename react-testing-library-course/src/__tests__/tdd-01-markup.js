6b917d495bf401c3641d6fa4a2e0cca61601dd37
mario-aj <mariojorge1997@mail.ru>
2022-05-23 16:45:42 +0100

    TDD of react form

---
+++ react-testing-library-course/src/__tests__/tdd-01-markup.js
@@ -0,0 +1,14 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react'
+import {render} from '@testing-library/react'
+
+import {Editor} from '../post-editor-01-markup'
+
+test('renders a form with title, content, tags and a submit button', () => {
+  const {getByLabelText, getByText} = render(<Editor />)
+
+  getByLabelText(/title/i)
+  getByLabelText(/content/i)
+  getByLabelText(/tags/i)
+  getByText(/submit/i)
+})
