36590ea8d56a653e40022c134fceb099a750ddbd
mario-aj <mariojorge1997@mail.ru>
2022-05-27 14:13:42 +0100

    config cypress baseUrl

--- jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
+++ jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
@@ -1,6 +1,6 @@
 describe('Calculator', () => {
   it('Can make calculations', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('/')
 
     cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)').click()
     cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)').click()
