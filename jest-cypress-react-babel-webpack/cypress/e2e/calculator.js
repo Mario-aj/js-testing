d0a4b631ff528516aeb50511d920855a6d31b495
mario-aj <mariojorge1997@mail.ru>
2022-05-27 14:36:26 +0100

    config and use tl/cypress commands to find* element async

--- jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
+++ jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
@@ -2,14 +2,11 @@ describe('Calculator', () => {
   it('Can make calculations', () => {
     cy.visit('/')
 
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)').click()
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(4)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)').click()
+    cy.findByText(/^1$/).click()
+    cy.findByText(/^\+$/).click()
+    cy.findByText(/^2$/).click()
+    cy.findByText(/^=$/).click()
 
-    cy.get('.css-lq9ahq-calculator-display--CalculatorDisplay').should(
-      'have.text',
-      '3',
-    )
+    cy.findByTestId(/total/i).should('have.text', '3')
   })
 })
